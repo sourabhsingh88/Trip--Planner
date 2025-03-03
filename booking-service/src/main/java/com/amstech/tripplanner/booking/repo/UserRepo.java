b783d8256a09c1cc982cc07e416041b49cb45ee9
Emarger <atulpatel6357@gmail.com>
2025-03-04 04:59:48 +0530

    Deleted all files except README.md

--- booking-service/src/main/java/com/amstech/tripplanner/booking/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.amstech.tripplanner.booking.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.amstech.tripplanner.booking.entity.*;
-public interface UserRepo extends JpaRepository<User, Integer>{
- 
-}
