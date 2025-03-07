d35b5ba16397d4b9a806aced35c02cc4eced4c4c
Emarger <atulpatel6357@gmail.com>
2025-03-07 11:43:39 +0530

    RoleAssigne API

---
+++ booking-service/src/main/java/com/amstech/tripplanner/booking/modal/request/UserRoleAssigneRequestModal.java
@@ -0,0 +1,20 @@
+package com.amstech.tripplanner.booking.modal.request;
+
+public class UserRoleAssigneRequestModal {
+
+	private int userId;
+	private int roleId;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	
+}
