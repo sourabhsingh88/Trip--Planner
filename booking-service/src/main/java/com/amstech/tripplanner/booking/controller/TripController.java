0d4d84a3f15f87cc4889ecc4b67334ef5c360ab1
Emarger <atulpatel6357@gmail.com>
2025-05-29 16:05:05 +0530

    cxkjb

--- booking-service/src/main/java/com/amstech/tripplanner/booking/controller/TripController.java
+++ booking-service/src/main/java/com/amstech/tripplanner/booking/controller/TripController.java
@@ -65,7 +65,7 @@ public class TripController {
 				if(image.getSize() > fileService.getFileMaxSize()) {
 					throw new Exception(" File Size Can Not Greater Than : " + fileService.getFileMaxSize() + " bytes");
 				}
-				filePath = fileService.saveFile(image.getBytes(),"trip",FilenameUtils.getExtension(image.getOriginalFilename()));
+				filePath = fileService.saveFile(image.getBytes(),"trips",FilenameUtils.getExtension(image.getOriginalFilename()));
 			}
 			
 			if (tripBanners != null) {
@@ -76,7 +76,7 @@ public class TripController {
 						throw new Exception(
 								"File size can not be gretter then: " + fileService.getFileMaxSize() + "byts");
 
-					tripBannerPath.add(fileService.saveFile(tripBanner.getBytes(), "users",FilenameUtils.getExtension(tripBanner.getOriginalFilename())));
+					tripBannerPath.add(fileService.saveFile(tripBanner.getBytes(), "tripBanners",FilenameUtils.getExtension(tripBanner.getOriginalFilename())));
 
 				}
 				
